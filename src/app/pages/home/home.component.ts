c788789bce11d288701a24b042e9098153e5206d
Vinícius Henrique Dias de Souza <vinihds@gmail.com>
2025-08-12 21:00:32 -0300

    criando json fake

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import gamesData from '../../data/dataFake.json';
+
+interface Game {
+  id: string;
+  gameLabel: string;
+  gameCover: string;
+  gamePrice: string;
+  gameType: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +15,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  games: Game[] = gamesData.games;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
